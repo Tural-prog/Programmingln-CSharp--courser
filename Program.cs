2d3220b694520cce7e48ec5fcfd466bacab37f64
Tural <turalprogramci@gmail.com>
2022-10-15 17:51:57 +0400

    Project initially added

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tapsiriq_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str ,close;
+            int i, soz;
+            Console.WriteLine("Metni daxil edin:\n");
+            str = Console.ReadLine();
+            i = 0;
+            soz = 1;
+            while (i <= str.Length - 1)
+            {
+                if (str[i] ==' ' || str[i] == '\n')
+                {
+                    soz++;
+                }
+                i++;
+        
+            }
+            Console.WriteLine("Metndeki sozlerin sayi:{0}\n", soz);
+            
+
+        }
+    }
+}
